19cea4a71a05af640dd20ac4a2c7271d1926c356
tk0418 <fwsoccer9418@gmail.com>
2023-12-02 17:26:36 +0900

    article detail commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,19 +6,14 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 
 class PostController extends Controller
-<<<<<<< HEAD
 { 
     public function index(Post $post)
     {
     return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
 }
-?>
-=======
-{
-    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
-{
-    return $post->get();//$postの中身を戻り値にする。
-}
-}
->>>>>>> main
+
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,15 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
-{
-    use HasFactory;
-<<<<<<< HEAD
+{use HasFactory;
     
-    public function getPaginateByLimit(int $limit_count = 2)
+    public function getPaginateByLimit(int $limit_count = 10)
 {
     // updated_atで降順に並べたあと、limitで件数制限をかける
     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
-}
-=======
->>>>>>> main
-}
+}}
