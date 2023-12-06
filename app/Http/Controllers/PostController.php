102211baf2171a04359836dd602bfbaa9471b794
tk0418 <fwsoccer9418@gmail.com>
2023-12-06 14:35:34 +0900

    ブログ投稿作成処理実行

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,15 +9,21 @@ class PostController extends Controller
 { 
     public function index(Post $post)
     {
-    return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
     }
     public function create()
-{
-    return view('posts.create');
-}
+    {
+        return view('posts.create');
+    }
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
 }
 
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
-{use HasFactory;
+
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'title',
+        'body',
+        ];
     
     public function getPaginateByLimit(int $limit_count = 10)
 {
